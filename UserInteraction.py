e62e89de7bd413c613a9667d5d7b10ebd3eb965d
PTPeraire <ptorrenp@hotmail.com>
2020-03-07 12:17:32 +0100

    change input to access directory

--- UserInteraction.py
+++ UserInteraction.py
@@ -46,21 +46,22 @@ def getUserInput():
     input_list = options.infile
     fasta_files = []
     pdb_files = []
-    if len(input_list) == 0: 
-        input_list = os.getcwd()
+    if len(input_list) == 0:
+        input_list = [os.getcwd()]
     for input in input_list:
         if os.path.isdir(input):
-            fasta_files.append([os.path.join(input, f) for f in os.listdir(input) if f.endswith(".fa") or f.endswith(".fasta")])
-            pdb_files.append([os.path.join(input, f) for f in os.listdir(input) if f.endswith(".pdb")])
+            print(os.listdir(input))
+            fasta_files = [os.path.join(input, f) for f in os.listdir(input) if f.endswith(".fa") or f.endswith(".fasta")]
+            pdb_files = [os.path.join(input, f) for f in os.listdir(input) if f.endswith(".pdb")]
         elif os.path.isfile(input) and (input.endswith(".fa") or input.endswith(".fasta")):
             fasta_files.append(input)
         elif os.path.isfile(input) and (input.endswith(".pdb")):
             pdb_files.append(input)
-        
+
 
     if len(fasta_files) == 0 and len(pdb_files) == 0:
         raise Exception("No fasta or pdb files were found")
     if len(fasta_files) == 0 or len(pdb_files) == 0:
         raise Exception("fasta or pdb file is missing")
-    
+
     return (fasta_files, pdb_files)
--- macrocomplex_builder.py
+++ macrocomplex_builder.py
@@ -8,3 +8,11 @@ import argparse, os, sys, UserInteraction
 #main function that is called when running the script
 if __name__ == "__main__":
     fasta_files, pdb_files = UserInteraction.getUserInput()
+    parser = PDB.PDBParser()
+    interact_structure = []
+    for pdb_struct in pdb_files:
+        interact_structure.append(parser.get_structure(pdb_struct,pdb_struct))
+
+    print(interact_structure)
+    # for element in pdb_struct:
+    #     print element.getst
