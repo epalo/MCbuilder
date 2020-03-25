acddf801aadc581bc48f98cf449b588f7968d849
anmeert <annikameert@protonmail.com>
2020-03-25 22:00:50 +0100

    add Interaction class, find start complex

--- macrocomplex_builder.py
+++ macrocomplex_builder.py
@@ -60,6 +60,21 @@ class Complex(object):
         # how to calculate z_score? 
         return 
 
+class Interaction():
+    def __init__(self, structure, chain_a, chain_b):
+        self.__structure = structure
+        self.__chain_a = chain_a
+        self.__chain_b = chain_b
+    
+    def get_structure(self):
+        return self.__structure 
+
+    def get_chain_a(self):
+        return self.__chain_a
+    
+    def get_chain_b(self):
+        return self.__chain_b
+    
 
 #main function that is called when running the script
 if __name__ == "__main__":
@@ -81,219 +96,226 @@ if __name__ == "__main__":
     parser = PDB.PDBParser()
     interactions = []
     # iterate through all pdb files and return a list of interaction objects
-    for pdb_file in pdb_files:
-        structure = parser.get_structure(pdb_file,pdb_file)
-        interactions.append(structure)
+    for i in range(len(pdb_files)):
+        structure = parser.get_structure(pdb_files[i],pdb_files[i])
+        ppb=PPBuilder()
+        # build the peptide to obtain the sequences of the chains
+        peptide = ppb.build_peptides(structure)
+        sequence_a = peptide[0].get_sequence()
+        sequence_b = peptide[1].get_sequence()
+        # build up the list of chains for each interaction 
+        chain_a, chain_b = structure.get_chains()
+        interacting_a = Interacting_Chain(chain_a, i, sequence_a, chain_b)
+        interacting_b = Interacting_Chain(chain_b, i, sequence_b,chain_a)
+        interactions.append(Interaction(structure, interacting_a, interacting_b))
     print(interactions)
 
-    # function to get all the chains of a list of interactions
-    def get_interacting_chains(interactions):
-        chains = []
-        for index in range(len(interactions)):
-            # build the peptide to obtain the sequences of the chains
-            ppb=PPBuilder()
-            peptide = ppb.build_peptides(interactions[index])
-            sequence_a = peptide[0].get_sequence()
-            sequence_b = peptide[1].get_sequence()
-            # build up the list of chains for each interaction 
-            chain_a, chain_b = interactions[index].get_chains()
-            chains.append(Interacting_Chain(chain_a, index, sequence_a, chain_b))
-            chains.append(Interacting_Chain(chain_b, index, sequence_b,chain_a))
-        return chains
 
+
+    # get all the chains of a list of interactions
+    chains = []
+    for interaction in interactions:
+        chains.append(interaction.get_chain_a())
+        chains.append(interaction.get_chain_b())
+    print(chains)
     log.info("PDB interactions processed")
-    chains = get_interacting_chains(interactions)
-    for chain in chains:
-        print(chain)
 
 # SEQUENCE ALIGNMENTS
 
 # find the sequences that occur multiple times in pdb files and save all proteins for each structural aln in a separate list
-sequences = []
-for i in range(len(chains)):
-    for m in range(i):
-        # just check sequence alignments if sequences are not in the same pair
-        if (chains[i].get_file_index() != chains[m].get_file_index()):
-            # find the best alignment for two sequences (first element of list of alignments)
-            alignment = pairwise2.align.globalxx(chains[i].get_sequence(), chains[m].get_sequence())[0]
-            aln_seq_1 = alignment[0]
-            aln_seq_2 = alignment[1]
-            al_length = len(alignment[0])
-            ident = sum(base1 == base2 for base1, base2 in zip(aln_seq_1, aln_seq_2))
-            if ident/al_length >= 0.95:
-                print("is identical")
-                inserted = True
-                for similar_seq in sequences:
-                    if chains[i] in similar_seq:
-                        if chains[m] not in similar_seq:
-                            similar_seq.append(chains[m])
+    sequences = []
+    for i in range(len(chains)):
+        for m in range(i):
+            # just check sequence alignments if sequences are not in the same pair
+            if (chains[i].get_file_index() != chains[m].get_file_index()):
+                # find the best alignment for two sequences (first element of list of alignments)
+                alignment = pairwise2.align.globalxx(chains[i].get_sequence(), chains[m].get_sequence())[0]
+                aln_seq_1 = alignment[0]
+                aln_seq_2 = alignment[1]
+                al_length = len(alignment[0])
+                ident = sum(base1 == base2 for base1, base2 in zip(aln_seq_1, aln_seq_2))
+                if ident/al_length >= 0.95:
+                    print("is identical")
+                    inserted = True
+                    for similar_seq in sequences:
+                        if chains[i] in similar_seq:
+                            if chains[m] not in similar_seq:
+                                similar_seq.append(chains[m])
+                                inserted = False
+                                break
+                        if chains[m] in similar_seq:
+                            if chains[i] not in similar_seq:
+                                similar_seq.append(chains[i])
+                                inserted = False
+                                break
+                        if chains[m] in similar_seq and chains[i] in similar_seq:
                             inserted = False
                             break
-                    if chains[m] in similar_seq:
-                        if chains[i] not in similar_seq:
-                            similar_seq.append(chains[i])
-                            inserted = False
-                            break
-                    if chains[m] in similar_seq and chains[i] in similar_seq:
-                        inserted = False
-                        break
-                if inserted:
-                    sequences.append([chains[i], chains[m]])
-                print(sequences)
-log.info(f"{len(sequences)} homologous chains found")
-for i in range(len(sequences)):
-    for el in sequences[i]:
-        print("elem", i, el.get_file_index())
+                    if inserted:
+                        sequences.append([chains[i], chains[m]])
+                    print(sequences)
+    log.info(f"{len(sequences)} homologous chains found")
+    for i in range(len(sequences)):
+        for el in sequences[i]:
+            print("elem", i, el.get_file_index())
 
-# HELPER FUNCTIONS
+    # HELPER FUNCTIONS
+    def get_similar_chains(chain):
+        for lst in sequences:
+            print("list", lst)
+            if chain in lst:
+                print("is in list", lst)
+                return lst
+        else:
+            return []
 
-def get_superimpose_options(current_complex):
-    superimpose_options = []
-    for chain in current_complex:  
-        for similar_seq in sequences:
-            if chain in similar_seq:
-                return superimpose_options.append(similar_seq)
+    def get_superimpose_options(current_complex):
+        superimpose_options = []
+        for chain in current_complex:  
+            for similar_seq in sequences:
+                if chain in similar_seq:
+                    return superimpose_options.append(similar_seq)
 
-# TODO: check both chains of starting complex and combine them to complete complex
+    # TODO: check both chains of starting complex and combine them to complete complex
 
-# final_complexes = []
-# def create_macrocomplex(current_complex, superimpose_chain, threshold):
-#     superimpose_options = get_superimpose_options(superimpose_chain)
-#     # no other superimposition options -> reached a leaf of the tree
-#     if not superimpose_options:
-#         # append to list of final complexes
-#         return superimpose(superimpose_chain,) 
-#      # reach threshold
-#     if (threshold == 0):
-#         # append to list of final complexes
-#         return 
-#     for chain_option in superimpose_options:
-#         # TODO: combine multiple superimpose options
-#         superimposition = superimpose(superimpose_chain, chain_option)
-#         created_complex = 
-#         current_rmsd = superimpose(superimpose_chain, chain_option)
-#         rmsd_threshold = 0
-#         if created_complex.rms < rmsd_threshold:
-#             if not is_clashing(current_complex, chain_option):
-#             #if not is_clashing2(created_complex):
-#                 create_macrocomplex(created_complex, chain_option.get_interaction(),threshold-1)
-
-
-# def create_macrocomplex(current_complex, next_chain, threshold):
-#     # gives us the new complex and the according rmsd
-#     superimposition = superimpose(superimpose_chain, next_chain)
-    
-#     superimpose_options = get_superimpose_options(superimposition[0],chains)
-#     # new complex has no other superimposition options -> reached a leaf of the tree
-#     if not superimpose_options:
-#         # append to list of final complexes
-#         return superimposition 
-#      # reach threshold
-#     if (threshold == 0):
-#         # append to list of final complexes
-#         return superimposition
-#     ## add here case for stoichometry
-#     if FALSE:
-#         return superimposition
-    
-#     for option in superimpose_options:
-#         created_complex = create_macrocomplex(created_complex, option ,threshold-1)
-#         current_structure = created_complex[]
-#         current_rmsd = created_complex[1]
-                
+    # final_complexes = []
+    # def create_macrocomplex(current_complex, superimpose_chain, threshold):
+    #     superimpose_options = get_superimpose_options(superimpose_chain)
+    #     # no other superimposition options -> reached a leaf of the tree
+    #     if not superimpose_options:
+    #         # append to list of final complexes
+    #         return superimpose(superimpose_chain,) 
+    #      # reach threshold
+    #     if (threshold == 0):
+    #         # append to list of final complexes
+    #         return 
+    #     for chain_option in superimpose_options:
+    #         # TODO: combine multiple superimpose options
+    #         superimposition = superimpose(superimpose_chain, chain_option)
+    #         created_complex = 
+    #         current_rmsd = superimpose(superimpose_chain, chain_option)
+    #         rmsd_threshold = 0
+    #         if created_complex.rms < rmsd_threshold:
+    #             if not is_clashing(current_complex, chain_option):
+    #             #if not is_clashing2(created_complex):
+    #                 create_macrocomplex(created_complex, chain_option.get_interaction(),threshold-1)
 
 
-def create_macrocomplex(current_complex, threshold):
-    superimpose_options = get_superimpose_options(current_complex[0])
-    best_complex = current_complex
-    # starting complex has no superimposition options
-    if not superimpose_options:
-        # then just return the starting complex
-        return best_complex
-    else:
-        for option in superimpose_options:
-            option_complex = superimpose(current_complex[0], option)
-            # no other superimposition options for the complex available (leaf)
-            # or reached threshold
-            # or TODO: ADD STOICHOMETRY option
-            if not get_superimpose_options(option_complex) or \
-                (threshold == 0) or \
-                    False:
-                # if Z-Score for option complex is lower than for the current best complex replace it
-                if option_complex.calc_z_score < best_complex.calc_z_score:
-                    best_complex = option_complex
-            # reach threshold
-            else:
-                # if we didn't reach the leaf yet, recursive call
-                create_macrocomplex(option_complex ,threshold-1)
-    return best_complex
+    # def create_macrocomplex(current_complex, next_chain, threshold):
+    #     # gives us the new complex and the according rmsd
+    #     superimposition = superimpose(superimpose_chain, next_chain)
         
-   
-   
-def is_clashing(current_complex, chain_to_superimpose):
-    backbone = {"CA", "C1\'"}
-    model_atoms = [atom for atom in current_complex.get_atoms() if atom.id in backbone]
-    chain_atoms = [atom for atom in chain_to_superimpose if atom.id in backbone]
-    n_search = PDB.NeighborSearch(model_atoms) # Generates a neigbour search tree
-    clashes = 0
-    for atom in chain_atoms:
-        clashes += bool(n_search.search(atom.coord, 1.7))  # If this atom shows clashes, add 1 to the clashes counter
-    if clashes/len(chain_atoms) >= 0.03:  # If more than 3% of atoms show clashes return yes
-        return True
-    else:  # Otherwise return no
-        return False
-
-# def is_clashing2(created_complex):
-#     clashes = detectClash(created_complex.get_atoms())
-#     sum = 0
-#     for clashDict in clashes.values():
-#         sum += len(clashDict)
-#     numClashes = sum/2
-#     if numClashes/len() >= 0.03:  # If more than 3% of atoms show clashes return yes
-#         return True
-#     else:  # Otherwise return no
-#         return False
+    #     superimpose_options = get_superimpose_options(superimposition[0],chains)
+    #     # new complex has no other superimposition options -> reached a leaf of the tree
+    #     if not superimpose_options:
+    #         # append to list of final complexes
+    #         return superimposition 
+    #      # reach threshold
+    #     if (threshold == 0):
+    #         # append to list of final complexes
+    #         return superimposition
+    #     ## add here case for stoichometry
+    #     if FALSE:
+    #         return superimposition
+        
+    #     for option in superimpose_options:
+    #         created_complex = create_macrocomplex(created_complex, option ,threshold-1)
+    #         current_structure = created_complex[]
+    #         current_rmsd = created_complex[1]
+                    
 
 
-def superimpose(current_complex, chain_b):
-    # TODO: only with backbone
-    # TODO: check all different positions ??? 
+    def create_macrocomplex(current_complex, threshold):
+        superimpose_options = get_superimpose_options(current_complex.get_structure())
+        best_complex = current_complex
+        # starting complex has no superimposition options
+        if not superimpose_options:
+            # then just return the starting complex
+            return best_complex
+        else:
+            for option in superimpose_options:
+                option_complex = superimpose(current_complex.get_structure(), option)
+                # no other superimposition options for the complex available (leaf)
+                # or reached threshold
+                # or TODO: ADD STOICHOMETRY option
+                if not get_superimpose_options(option_complex) or \
+                    (threshold == 0) or \
+                        False:
+                    # if Z-Score for option complex is lower than for the current best complex replace it
+                    if option_complex.calc_z_score < best_complex.calc_z_score:
+                        best_complex = option_complex
+                # reach threshold
+                else:
+                    # if we didn't reach the leaf yet, recursive call
+                    create_macrocomplex(option_complex ,threshold-1)
+        return best_complex
+            
+    
+    
+    def is_clashing(current_complex, chain_to_superimpose):
+        backbone = {"CA", "C1\'"}
+        model_atoms = [atom for atom in current_complex.get_atoms() if atom.id in backbone]
+        chain_atoms = [atom for atom in chain_to_superimpose if atom.id in backbone]
+        n_search = PDB.NeighborSearch(model_atoms) # Generates a neigbour search tree
+        clashes = 0
+        for atom in chain_atoms:
+            clashes += bool(n_search.search(atom.coord, 1.7))  # If this atom shows clashes, add 1 to the clashes counter
+        if clashes/len(chain_atoms) >= 0.03:  # If more than 3% of atoms show clashes return yes
+            return True
+        else:  # Otherwise return no
+            return False
 
-    # get superimposition positions in the current complex
-    superimposition_positions = []
+    # def is_clashing2(created_complex):
+    #     clashes = detectClash(created_complex.get_atoms())
+    #     sum = 0
+    #     for clashDict in clashes.values():
+    #         sum += len(clashDict)
+    #     numClashes = sum/2
+    #     if numClashes/len() >= 0.03:  # If more than 3% of atoms show clashes return yes
+    #         return True
+    #     else:  # Otherwise return no
+    #         return False
 
-    superimp = PDB.Superimposer()
-    best_superimposition = None
-    best_rmsd = 10
-    for chain in superimposition_positions:
-        atoms_a = []
-        atoms_b = []
-        for elem in chain.get_atoms():
-            atoms_a.append(elem)
-        for elem in chain_b.get_atoms():
-            atoms_b.append(elem)
-        # setting fixed and moving atoms 
-        superimp.set_atoms(atoms_a, atoms_b)
-        # apply the superimposition, TODO: think about copy??
-        superimposition = superimp.apply(atoms_b) 
-        rmsd = superimp.rms
-        if rmsd < best_rmsd:
-            best_superimposition = superimposition
-            best_rmsd = rmsd
-    created_complex = Complex(best_superimposition, current_complex.get_chains().append(chain_b))
-    return created_complex
 
-# BUILDING UP THE COMPLEX
+    def superimpose(current_complex, chain_b):
+        # TODO: only with backbone
+        # TODO: check all different positions ??? 
 
-starting_interaction = interactions[0]
-interaction_sum = 0
-# find pdb-file with the most interactions
-for interaction in interactions:
-    chain_a, chain_b = interaction.get_chains()
-    sum = chain_a + chain_b
-    if sum > interaction_sum :
-        starting_interaction = interaction
-starting_complex = Complex(starting_interaction, [starting_interaction.get_chain_a, starting_interaction.get_chain_b])
-create_macrocomplex(starting_complex, 100)
+        # TODO: get superimposition positions in the current complex
+        superimposition_positions = []
 
-# TODO: check for DNA 
+        superimp = PDB.Superimposer()
+        best_superimposition = None
+        best_rmsd = 10
+        for chain in superimposition_positions:
+            atoms_a = []
+            atoms_b = []
+            for elem in chain.get_atoms():
+                atoms_a.append(elem)
+            for elem in chain_b.get_atoms():
+                atoms_b.append(elem)
+            # setting fixed and moving atoms 
+            superimp.set_atoms(atoms_a, atoms_b)
+            # apply the superimposition, TODO: think about copy??
+            superimposition = superimp.apply(atoms_b) 
+            rmsd = superimp.rms
+            if rmsd < best_rmsd:
+                best_superimposition = superimposition
+                best_rmsd = rmsd
+        created_complex = Complex(best_superimposition, current_complex.get_chains().append(chain_b))
+        return created_complex
+
+    # BUILDING UP THE COMPLEX
+
+    starting_interaction = None
+    interaction_sum = 0
+    # find pdb-file with the most interactions
+    for interaction in interactions:
+        sum = len(get_similar_chains(interaction.get_chain_a())) + len(get_similar_chains(interaction.get_chain_b()))
+        if sum > interaction_sum :
+            starting_interaction = interaction
+            interaction_sum = sum
+    starting_complex = Complex(starting_interaction, [starting_interaction.get_chain_a(), starting_interaction.get_chain_b()])
+    create_macrocomplex(starting_complex, 100)
+
+    # TODO: check for DNA
