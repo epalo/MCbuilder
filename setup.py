01a39e4a9fdbaedb68a309fd641aa546e0cf1220
epalo <eparejalorente@gmail.com>
2020-04-03 22:05:00 +0200

    source

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name='mcbuilder',
     version='0.1.0',
     description='A macrocomplex builder from pdb interaction files',
-    url='https://github.com/anmeert/sbi-project',
+    url='https://github.com/anmeert/MCbuilder',
     author='Annika, Elena and Paula',
     keywords='protein pdb macrocomplex',
     packages=['src'], # Creo que es como una carpeta donde tienes todos los scripts
-    scripts=['macrocomplex_builder.py'], # Creo que aqui hay que poner los scripts necesarios para correr el macrocomplex_builder
+    scripts=['src/macrocomplex_builder.py'], # Creo que aqui hay que poner los scripts necesarios para correr el macrocomplex_builder
     license='',
     long_description=open('README.md').read(),
     install_requires=[
